5f821105fd52d6fc973ee8b8bd34f4e0401c2ae8
janWICKEDpro <janroyal45@gmail.com>
2023-06-09 03:55:54 +0100

    fonts and colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const pink = Color(0xffFE3675);
+  static const brown = Color(0xffFF7854);
+  static const darkGrey = Color(0xff4A4A4A);
+  static const grey = Color(0xff939393);
+}
