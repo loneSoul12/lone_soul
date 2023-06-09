db9c1d03eee530257cc169c042586df7cfafd398
janWICKEDpro <janroyal45@gmail.com>
2023-06-09 04:06:33 +0100

    text style packages

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -1,1 +1,18 @@
-class AppStyles {}
+import 'package:flutter/material.dart';
+import 'package:lone_soul/app_colors.dart';
+
+class AppStyles {
+  static TextStyle boldHeader = const TextStyle(
+    fontFamily: 'DmSans',
+    fontSize: 30,
+    color: AppColors.darkGrey,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle text = const TextStyle(
+    fontFamily: 'DmSans',
+    fontSize: 15,
+    color: AppColors.darkGrey,
+    fontWeight: FontWeight.w700,
+  );
+}
