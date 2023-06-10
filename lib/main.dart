9efeb4f22347800f6d976c480516ee51cd03a54f
janWICKEDpro <janroyal45@gmail.com>
2023-06-10 09:22:47 +0100

    supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lone_soul/app_colors.dart';
 import 'package:lone_soul/screens/stepper_screens/main_stepper.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://kbduhbutbqorrmbesabf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiZHVoYnV0YnFvcnJtYmVzYWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzODQ4ODQsImV4cCI6MjAwMTk2MDg4NH0.f3gnYl62dtbYgU-cNQrdLuCxIPb6RHJMHYzq2qcJ_Po',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
